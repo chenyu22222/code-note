152aa7a61203c0c76c39690ddebd88a8f22a4a97
shilinlee <836160610@qq.com>
2019-05-15 22:45:39 +0800

    fix 判断闰年应该使用过期时间的年字段

--- python/order/test_order_expiration_date.py
+++ python/order/test_order_expiration_date.py
@@ -14,6 +14,16 @@ def test_expired_in_feb_date():
     assert get_expiration_date(2017, 1, 30) == [2017, 2, 28]
 
 
+def test_order_3_months_to_feb_date(mocker):
+    mocker.patch("order_expiration_date.ORDER_MONTHS", 3)
+    assert get_expiration_date(2015, 11, 30) == [2016, 2, 29]
+
+
+def test_order_3_months_to_feb_date2(mocker):
+    mocker.patch("order_expiration_date.ORDER_MONTHS", 3)
+    assert get_expiration_date(2016, 11, 29) == [2017, 2, 28]
+
+
 def test_order_3_months_date(mocker):
     mocker.patch("order_expiration_date.ORDER_MONTHS", 3)
     assert get_expiration_date(2017, 1, 30) == [2017, 4, 30]
