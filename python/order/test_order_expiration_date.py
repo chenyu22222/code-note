69d5177d7b01292ade933817e3130649c90dfb00
shilinlee <shilinlee.ghost@gmail.com>
2019-05-15 19:41:20 +0800

    add requirements.txt and test case

--- python/order/test_order_expiration_date.py
+++ python/order/test_order_expiration_date.py
@@ -14,5 +14,20 @@ def test_expired_in_feb_date():
     assert get_expiration_date(2017, 1, 30) == [2017, 2, 28]
 
 
+def test_order_3_months_date(mocker):
+    mocker.patch("order_expiration_date.ORDER_MONTHS", 3)
+    assert get_expiration_date(2017, 1, 30) == [2017, 4, 30]
+
+
+def test_order_6_months_date(mocker):
+    mocker.patch("order_expiration_date.ORDER_MONTHS", 6)
+    assert get_expiration_date(2017, 2, 28) == [2017, 8, 28]
+
+
+def test_order_12_months_date(mocker):
+    mocker.patch("order_expiration_date.ORDER_MONTHS", 12)
+    assert get_expiration_date(2017, 1, 1) == [2018, 1, 1]
+
+
 if __name__ == '__main__':
     pytest.main()
