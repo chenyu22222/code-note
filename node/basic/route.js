6e7871c996b528d639cd7e8d44fdee65f85d3277
shilinlee <836160610@qq.com>
2019-05-26 15:13:10 +0800

    node config route

--- node/basic/route.js
+++ node/basic/route.js
@@ -4,7 +4,20 @@ const config = require("./config").config
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content_Type", "text/plain");
-    res.end("hello world\n");
+    
+    switch(req.url){
+        case "/": {
+            res.end("hello world\n");
+            break;
+        };
+        case "/about": res.end("This is about page.\n");
+            break;
+        case "/home": res.end("This is home page!");
+            break;
+        default:  res.end("not found.");
+    }
+    
+    
 })
 
 server.listen(config.port, config.hostname, ()=> {
